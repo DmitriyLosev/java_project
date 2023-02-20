121502e5036486179be5b5fcc29ff8f6a694d729
Dmitriy Bauman <dmitriy.losev@icloud.com>
2023-02-20 21:16:48 +0300

    task for seminar3

---
+++ Seminar3/mergeSorting.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+public class mergeSorting {
+    public static void main(String[] args) {
+        int[] array = { 9, 5, 7, 3, 2, 4, 1, 6, 8 };
+        int[] result = mergesort(array);
+        System.out.println("Массив беспорядочных чисел:" + Arrays.toString(array));
+        System.out.print("Массив отсортированный:" + Arrays.toString(result));
+    }
+    public static int[] mergesort(int[] array) {
+        int[] buffer1 = Arrays.copyOf(array, array.length);
+        int[] buffer2 = new int[array.length];
+        int[] result = mergesort(buffer1, buffer2, 0, array.length);
+        return result;
+    }
+
+    public static int[] mergesort(int[] buffer1, int[] buffer2,
+                                       int oneIndex, int finIndex) {
+        if (oneIndex >= finIndex - 1) {
+            return buffer1;
+        }
+
+        int middle = oneIndex + (finIndex - oneIndex) / 2;
+        int[] sorted1 = mergesort(buffer1, buffer2, oneIndex, middle);
+        int[] sorted2 = mergesort(buffer1, buffer2, middle, finIndex);
+
+        // Слияние
+        int index1 = oneIndex;
+        int index2 = middle;
+        int destIndex = oneIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < finIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++]
+                    : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < finIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+}
