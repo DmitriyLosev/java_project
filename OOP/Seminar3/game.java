5060abf585e1dc762a01117e6bd53b4007e04d79
Dmitriy Bauman <dmitriy.losev@icloud.com>
2023-03-14 08:18:25 +0300

    classic version 1.0

--- OOP/Seminar3/game.java
+++ OOP/Seminar3/game.java
@@ -1,125 +1,61 @@
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Random;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.util.Scanner;
 
-public class BullsAndCowsGameUI extends JFrame {
-    private static final long serialVersionUID = 1L;
+public class BullsAndCowsGame {
     private static final int SECRET_NUMBER_LENGTH = 4;
-    private static final int MAX_ATTEMPTS = 10;
-
-    private int attemptsLeft;
     private String secretNumber;
-    private JTextField guessTextField;
-    private JLabel resultLabel;
-    private JButton guessButton;
-
-    public BullsAndCowsGameUI() {
-        super("Bulls and Cows Game");
-
-        setLayout(new BorderLayout());
-
-        JPanel topPanel = new JPanel(new GridLayout(2, 2));
-        topPanel.add(new JLabel("Secret Number:"));
-        topPanel.add(new JLabel(""));
-        topPanel.add(new JLabel("Attempts Left:"));
-        topPanel.add(new JLabel(""));
-        add(topPanel, BorderLayout.NORTH);
-
-        JPanel centerPanel = new JPanel(new GridLayout(2, 1));
-        centerPanel.add(new JTextField(SECRET_NUMBER_LENGTH));
-        guessTextField = new JTextField(SECRET_NUMBER_LENGTH);
-        guessTextField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                guessButton.doClick();
-            }
-        });
-        centerPanel.add(guessTextField);
-        add(centerPanel, BorderLayout.CENTER);
-
-        JPanel bottomPanel = new JPanel(new BorderLayout());
-        guessButton = new JButton("Guess");
-        guessButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (attemptsLeft == 0) {
-                    JOptionPane.showMessageDialog(BullsAndCowsGameUI.this, "You have no attempts left.");
-                    return;
-                }
-                String guess = guessTextField.getText();
-                if (guess.length() != SECRET_NUMBER_LENGTH) {
-                    JOptionPane.showMessageDialog(BullsAndCowsGameUI.this, "Guess should be " + SECRET_NUMBER_LENGTH + " digits long.");
-                    return;
-                }
-                int bulls = 0;
-                int cows = 0;
-                for (int i = 0; i < SECRET_NUMBER_LENGTH; i++) {
-                    char c = guess.charAt(i);
-                    if (secretNumber.charAt(i) == c) {
-                        bulls++;
-                    } else if (secretNumber.indexOf(c) != -1) {
-                        cows++;
-                    }
-                }
-                if (bulls == SECRET_NUMBER_LENGTH) {
-                    JOptionPane.showMessageDialog(BullsAndCowsGameUI.this, "Congratulations! You guessed the secret number in " + (MAX_ATTEMPTS - attemptsLeft + 1) + " attempts.");
-                    resetGame();
-                } else {
-                    attemptsLeft--;
-                    resultLabel.setText("Bulls: " + bulls + " Cows: " + cows);
-                    topPanel.getComponent(1).setText(secretNumber);
-                    topPanel.getComponent(3).setText("" + attemptsLeft);
-                    guessTextField.setText("");
-                }
-                if (attemptsLeft == 0) {
-                    JOptionPane.showMessageDialog(BullsAndCowsGameUI.this, "You have no attempts left.");
-                    resetGame();
-                }
-            }
-        });
-        bottomPanel.add(guessButton, BorderLayout.WEST);
-        resultLabel = new JLabel("");
-        bottomPanel.add(resultLabel, BorderLayout.CENTER);
-        add(bottom Panel, BorderLayout.SOUTH);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 150);
-        setLocationRelativeTo(null);
-        setVisible(true);
-
-        resetGame();
-    }
-
-    private void resetGame() {
-        attemptsLeft = MAX_ATTEMPTS;
-        secretNumber = generateSecretNumber();
-        resultLabel.setText("");
-        guessTextField.setText("");
-        topPanel.getComponent(1).setText("");
-        topPanel.getComponent(3).setText("" + attemptsLeft);
-    }
-
-    private String generateSecretNumber() {
-        Random random = new Random();
-        StringBuilder sb = new StringBuilder();
-        while (sb.length() < SECRET_NUMBER_LENGTH) {
-            int digit = random.nextInt(10);
-            if (sb.indexOf("" + digit) == -1) {
-                sb.append(digit);
-            }
-        }
-        return sb.toString();
-    }
 
     public static void main(String[] args) {
-        new BullsAndCowsGameUI();
+        BullsAndCowsGame game = new BullsAndCowsGame();
+        game.start();
     }
 
+    public void start() {
+        generateSecretNumber();
+        Scanner scanner = new Scanner(System.in);
+        int attemptsLeft = 10;
+        while (true) {
+            System.out.println("Введите " + SECRET_NUMBER_LENGTH + "-значное число (у вас есть " + attemptsLeft + " попыток):");
+            String guess = scanner.next();
+            if (guess.length() != SECRET_NUMBER_LENGTH) {
+                System.out.println("Пожалуйста введитн " + SECRET_NUMBER_LENGTH + "-значное число.");
+                continue;
+            }
+            int bulls = 0;
+            int cows = 0;
+            for (int i = 0; i < SECRET_NUMBER_LENGTH; i++) {
+                char c = guess.charAt(i);
+                if (c == secretNumber.charAt(i)) {
+                    bulls++;
+                } else if (secretNumber.indexOf(c) != -1) {
+                    cows++;
+                }
+            }
+            attemptsLeft--;
+            if (bulls == SECRET_NUMBER_LENGTH) {
+                System.out.println("Вы победили!");
+                break;
+            } else if (attemptsLeft == 0) {
+                System.out.println("Вы проиграли. Загаданное число " + secretNumber + ".");
+                break;
+            } else {
+                System.out.println(bulls + " Быки и " + cows + " Коровы. Попробуйте снова.");
+            }
+        }
+    }
 
+    private void generateSecretNumber() {
+        Random random = new Random();
+        boolean[] used = new boolean[10];
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < SECRET_NUMBER_LENGTH; i++) {
+            int digit;
+            do {
+                digit = random.nextInt(10);
+            } while (used[digit]);
+            used[digit] = true;
+            builder.append(digit);
+        }
+        secretNumber = builder.toString();
+    }
+}
